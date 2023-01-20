c2fe158d7364c6ca8d9ea68bc204ead1ec42f224
Noamshochat <88544651+zusha1992@users.noreply.github.com>
2023-01-20 11:40:57 +0200

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -13,3 +13,25 @@ def canReach(s, minJump, maxJump):
                     return True
         farthest = i + maxJump
     return False
+
+
+# https://leetcode.com/problems/jump-game-iii/
+# 1306. Jump Game III
+# Time Complexity - O(n).
+# Memory - O(n).
+def canReach(self, arr: List[int], start: int) -> bool:
+  visited = [False] * len(arr)
+    left, right = 0, len(arr) - 1
+    neighbors = deque([start])
+    while neighbors:
+        i = neighbors.popleft()
+        if arr[i] == 0:
+            return True
+        else:
+            visited[i] = True
+        jump_forword, jump_backwords = i + arr[i], i - arr[i]
+        if left <= jump_forword <= right and not visited[jump_forword]:
+            neighbors.append(jump_forword)
+        if left <= jump_backwords <= right and not visited[jump_backwords]:
+            neighbors.append(jump_backwords)
+    return False
